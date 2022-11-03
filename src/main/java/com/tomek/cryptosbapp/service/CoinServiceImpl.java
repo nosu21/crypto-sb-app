4ec23bef8f890a06f9e1adffda6a0898c3914f31
tomasz.nosal <tnosal@future-processing.com>
2022-11-03 10:58:43 +0100

    some improvements

--- src/main/java/com/tomek/cryptosbapp/service/CoinServiceImpl.java
+++ src/main/java/com/tomek/cryptosbapp/service/CoinServiceImpl.java
@@ -9,7 +9,6 @@ import com.tomek.cryptosbapp.repository.CoinRepository;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
-import lombok.SneakyThrows;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
